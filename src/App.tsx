999babdf1aed6696a399ccd8aec0d8aaae6bdb2e
pranayande-ux <pranayande1032@gmail.com>
2025-09-25 14:55:07 +0530

    Fix imports for Vercel Linux build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { Toaster } from "./components/ui/toaster";
-import { Toaster as Sonner } from "./components/ui/sonner";
+// src/App.tsx
+
+import Toaster from "./components/ui/toaster";
+import Sonner from "./components/ui/sonner";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -25,4 +27,3 @@ const App = () => (
 );
 
 export default App;
-
